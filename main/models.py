bda1850498e161e73dfd0987423c69016c97dca8
Frasha <frashasanto111@gmail.com>
2024-06-09 13:34:43 +0300

    transcations get saved to the database

--- main/models.py
+++ main/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# models.py
-from django.db import models
-
 class Transaction(models.Model):
     transaction_id = models.CharField(max_length=50, unique=True)
     amount = models.FloatField()
